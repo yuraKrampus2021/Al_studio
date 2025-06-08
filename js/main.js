fad949edfba201ac1d8518f793cb34defb52fb4f
Yurii Kramar <y.kramar15@gmail.com>
2025-06-08 16:56:22 +0200

    Updates

--- js/main.js
+++ js/main.js
@@ -1,6 +1,11 @@
 const burgerBtn = document.querySelector('.btn-menu')
 const menu = document.querySelector('.menu')
+const closeBtn = document.querySelector('.close');
 
 burgerBtn.addEventListener('click', () => {
     menu.classList.add('active');
+})
+
+closeBtn.addEventListener('click', () => {
+    menu.classList.remove('active');
 })
